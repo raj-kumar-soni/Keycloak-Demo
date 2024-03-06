276f632a460eae1ca3af165363c60cf94d97014e
Raj Kumar Soni <rk.soni6461@gmail.com>
2024-03-06 22:38:46 +0530

    First Commit

---
+++ src/main/java/com/example/demo/SampleTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleTestingApplication.class, args);
+	}
+
+}
